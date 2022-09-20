2c4d69c77fdf56e095fac547e0b9daf7a8979247
Susmit <walekar02@gmail.com>
2022-09-20 09:44:17 +0530

    Initial commit

---
+++ shop/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+  
+ 
+    path("products/<int:myid>", views.productView, name="ProductView"),
+   
+  
+
+]
